fa42051657980eb5bc26d00056af644444394a48
Andrei <andrei.vupt@gmail.com>
2025-07-14 21:52:20 -0300

    feat: users find

---
+++ src/users/user.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all users (admin only)' })
+  @ApiResponse({ status: 200, description: 'List of users' })
+  @Get()
+  async findAll(@Req() req) {
+    return this.usersService.findAll(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiResponse({ status: 200, description: 'User details' })
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Req() req) {
+    return this.usersService.findOne(parseInt(id), req.user);
+  }
+}
